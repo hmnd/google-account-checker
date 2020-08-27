9dbf4c982e464a9b8ffbc3fed4a1129eefe43ad9
David Roizenman <david@hmnd.io>
2020-08-27 00:37:45 -0700

    Update requirements.txt

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from time import sleep
 import typer
 from pathlib import Path
 from googleaccountchecker import GoogleAccountChecker
+from urllib.parse import urlsplit, parse_qs
+from enum import Enum
+
+
+class FileFormat(str, Enum):
+    txt = "txt"
+    csv = "csv"
 
 
 def process_household(df, checker: GoogleAccountChecker):
@@ -29,15 +36,25 @@ def main(
     cookies_path: Path = typer.Option(
         None, help="Path to cookies.txt", file_okay=True, dir_okay=False
     ),
-    docs_id: str = typer.Option(None, help="ID of any Google Doc in your account"),
+    google_doc: str = typer.Option(
+        None, help="URL or ID of any Google Doc in your account"
+    ),
+    out_format: FileFormat = typer.Option(
+        FileFormat.txt, help="Format of output file", case_sensitive=False
+    ),
 ):
+    if google_doc.startswith("http"):
+        split_url = urlsplit(google_doc)
+        docs_id = str(dict(parse_qs(split_url.query)).get("id"))
+    else:
+        docs_id = google_doc
     print("Processing...")
     checker = GoogleAccountChecker(cookies_path)
     checker.setup(docs_id)
     tqdm.pandas(desc="Processing Households")
     file_out = os.path.join(
         os.path.dirname(os.path.realpath(input_file)),
-        os.path.splitext(input_file)[0] + " - Google PCR Import.txt",
+        os.path.splitext(input_file)[0] + f" - Google PCR Import.{out_format}",
     )
     emails_df = pd.read_csv(input_file, delimiter=",", dtype=str).copy()
     emails_df.rename(columns={"Household Id": "lookup_household_id"}, inplace=True)
@@ -45,7 +62,11 @@ def main(
 
     out = emails_df.progress_apply(process_household, args=(checker,), axis=1)
     print("Exporting results to CSV...")
-    out.to_csv(file_out, sep="\t", index=False)
+    if out_format == FileFormat.txt:
+        out_separator = "\t"
+    else:
+        out_separator = ","
+    out.to_csv(file_out, sep=out_separator, index=False)
     print("All done! View file at {}".format(file_out))
 
 
